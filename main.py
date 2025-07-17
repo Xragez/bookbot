a4b1b1cba3edbe79307e21af8639c3200f21a649
Paweł Zegarski <pawelzeg@gmail.com>
2025-07-17 09:47:17 +0200

    add console raport and filepath as cli argument

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-from stats import get_book_text, count_words, count_characters
+from stats import get_book_text, count_words, count_characters, get_char_count_list
+import sys
+
+def print_report(filepath, book_contents):
+    word_count = count_words(book_contents)
+    characters = count_characters(book_contents)
+    char_counts = get_char_count_list(characters)
+
+    print("{============ BOOKBOT ============")
+    print(f"Analyzing book found {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_dict in char_counts:
+        if char_dict['char'].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
 
 def main():
-    book_contents = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    book_contents = get_book_text(filepath)
     # print(book_contents)
-    num_words = count_words(book_contents)
-    print(f"{num_words} words found in the document")
-    char_dict = count_characters(book_contents)
-    print(char_dict)
+    # num_words = count_words(book_contents)
+    # print(f"{num_words} words found in the document")
+    # char_dict = count_characters(book_contents)
+    # print(char_dict)
+
+    print_report(filepath, book_contents)
 
 main()
--- stats.py
+++ stats.py
@@ -9,11 +9,23 @@ def count_words(text):
 
 def count_characters(text):
     characters = {}
+
     for char in text:
         lowered_char = char.lower()
+
         if lowered_char not in characters:
             characters[lowered_char] = 1
         else:
             characters[lowered_char] += 1
     return characters
-        
+
+def get_char_count_list(characters):
+    char_list = []
+    for char, count in characters.items():
+        if char.isalpha():
+            char_list.append({
+                'char': char,
+                'num': count
+            })
+    char_list.sort(key=lambda x: x['num'], reverse=True)
+    return char_list
